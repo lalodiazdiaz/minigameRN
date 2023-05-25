28cb558f20c2a5b6d4db60fa355681c0729e45e0
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-05-25 16:45:17 -0600

    buttons

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,35 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+  },
+});
